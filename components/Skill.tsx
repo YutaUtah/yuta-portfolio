c741de4f567fc12b0324f5e6bede2123266be905
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-09-19 00:33:41 +0900

    adjusted hover round size for md

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -11,7 +11,7 @@ export default function Skill({}: Props) {
         transition={{duration:1}}
         whileInView={{opacity:1, x:0 }}
         src="https://www.humanrightslogo.net/sites/default/files/styles/download_preview/public/images/preview/HRLogoCMYKsmall.png?itok=yn3lBDJC"
-        className='rounded-full border border-gray-500 w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 w-24 md:w-28 md:h-28 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
         <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
             <div className='flex items-center justify-center h-full'>
