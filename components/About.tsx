639c866eae7be376192ef50a925bde269d311124
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-09-21 22:32:52 +0900

    replaced with dynamic data

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { PageInfo } from "../typings";
+import { urlFor } from "../sanity";
 
-type Props = {};
+type Props = {
+  pageInfo: PageInfo;
+};
 
-export default function About({}: Props) {
+export default function About({ pageInfo }: Props) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -29,20 +33,12 @@ export default function About({}: Props) {
         viewport={{
           once: true,
         }}
-        src="https://vikings.lawrence.edu/images/2020/4/23//yuta_hayashi_msoc_2018_19.jpg"
+        src={urlFor(pageInfo?.profilePic).url()}
         className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
       />
       <div className="space-y-10 px-0 md:px-10">
         <h4 className="text-4xl font-semibold">About Myself</h4>
-        <p className="text-base">
-          I grew up in Tokyo, but I decided to seek my bachelor degree in the
-          United States. I graduated from Lawrence University in Wisconsin. I
-          majored in Mathematics and minored in Physics and Anthropology.
-          Currently, I am working at J.P. Morgan Tokyo Office as a software
-          engineer. My interest is backend software development. My hobby
-          includes playing soccer and piano. Recently, I get to own a classic
-          car from my father, so I am enjoying maintaining it and driving!
-        </p>
+        <p className="text-base">{pageInfo?.backgroundInformation}</p>
       </div>
     </motion.div>
   );
